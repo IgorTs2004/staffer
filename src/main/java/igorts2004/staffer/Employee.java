a66f2d76c7e8151e17461474eb261e56139da695
igorts2004 <igorts2004@gmail.com>
2014-08-10 16:16:05 +0300

    some files wasn't commited

--- src/main/java/igorts2004/staffer/Employee.java
+++ src/main/java/igorts2004/staffer/Employee.java
@@ -2,7 +2,21 @@ package igorts2004.staffer;
 
 public class Employee {
 
+	private int id;
 	private String name;
+	
+	public Employee(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
