b53974687079685442a9bd851a6273ce0faab490
igorts2004 <igorts2004@gmail.com>
2014-08-10 11:59:19 +0300

    bean added and extracted fron container

---
+++ src/main/java/igorts2004/staffer/Employee.java
@@ -0,0 +1,15 @@
+package igorts2004.staffer;
+
+public class Employee {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
