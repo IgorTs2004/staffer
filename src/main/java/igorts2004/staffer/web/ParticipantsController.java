002c387e839f5c4b52d27a2399ed77dc07094179
igorts2004 <igorts2004@gmail.com>
2014-08-21 15:49:05 +0300

    WIP: editing project

--- src/main/java/igorts2004/staffer/web/ParticipantsController.java
+++ src/main/java/igorts2004/staffer/web/ParticipantsController.java
@@ -2,6 +2,7 @@ package igorts2004.staffer.web;
 
 import java.util.List;
 
+import igorts2004.staffer.domain.Employee;
 import igorts2004.staffer.domain.Project;
 import igorts2004.staffer.service.StafferService;
 
@@ -13,12 +14,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("project")
 public class ParticipantsController {
 
 	@Autowired
 	private StafferService stafferService;
+
+	@ModelAttribute("allEmployees")
+	public List<Employee> populateEmployees() {
+		return stafferService.getEmployeeList();
+	}
 
 	@RequestMapping(value = "/participants", method = RequestMethod.GET)
 	public String setupForm(@RequestParam("projectId") Long projectId,
@@ -28,19 +36,18 @@ public class ParticipantsController {
 		return "participants";
 	}
 
-	/*
-	 * @RequestMapping(value = "/participants/add", method = RequestMethod.POST)
-	 * public String addContact(@ModelAttribute("project") Project project,
-	 * BindingResult result) { stafferService.addProject(project);
-	 * 
-	 * return "redirect:/project"; }
-	 */
+	@RequestMapping(value = "/participants/add", method = RequestMethod.POST)
+	public String addContact(@ModelAttribute("project") Project project,
+			BindingResult result) {
+		//stafferService.addParticipant(project);
+		return "redirect:/participants?projectId=" + project.getId();
+	}
 
 	@RequestMapping(value = "/participants/remove", method = RequestMethod.GET)
 	public String removeParticipant(@RequestParam("projectId") Long projectId,
 			@RequestParam("employeeId") Long employeeId) {
 		stafferService.removeParticipant(projectId, employeeId);
-		return "redirect:/participants?projectId="+projectId;
+		return "redirect:/participants?projectId=" + projectId;
 	}
 
 }
--- src/main/webapp/WEB-INF/jsp/participants.jsp
+++ src/main/webapp/WEB-INF/jsp/participants.jsp
@@ -10,6 +10,22 @@
 <body>
 	<h2>Manage Participants</h2>
 
+	<form:form method="post" action="participants/add" modelAttribute="project">
+		<table>
+			<tr>
+				<td>Project Name</td>
+				<td><form:input path="name" /></td>
+			</tr>
+			<tr>
+				<td>Participants</td>
+				<td><form:select path="participants" multiple="true" items="${allEmployees}"
+						itemValue="id" itemLabel="name" /></td>
+			</tr>
+			<tr>
+				<td colspan="2"><input type="submit" value="Add" /></td>
+			</tr>
+		</table>
+	</form:form>
 
 	<table border="1">
 		<tr>
